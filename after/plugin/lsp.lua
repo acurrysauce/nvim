de402ed03ede719323942811db01134cafcc6120
Aaron Curry <acurrysauce@gmail.com>
2023-07-23 20:36:10 -0700

    change autocomplete binding
    change autocomplete binding

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -23,9 +23,9 @@ lsp.configure('lua_ls', {
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-		['<C-CR>'] = cmp.mapping.confirm({ select = true }),
+--		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+--		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
 		['<C-Space>'] = cmp.mapping.complete(),
 })
 
