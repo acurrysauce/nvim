c350756b2a4471235cbe0f22cb53950cf0301419
Aaron Curry <acurrysauce@gmail.com>
2023-07-23 20:47:50 -0700

    progress on setting up set.lua
    progress of setting up set.lua

--- lua/curryberto/set.lua
+++ lua/curryberto/set.lua
@@ -1,8 +1,22 @@
+--enable line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-
+--indenting stuff
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+--line wrapping is kind of bad
+vim.opt.wrap = false
+
+--backup undo dir so that undo tree can persist across sessions
+--vim.opt.swapfile = false
+--vim.opt.backup = false
+--vim.opt.undofile = true
+--vim.opt.undodir = os.getenv("Home") .. "/.vim/undodir"
 
 
 
